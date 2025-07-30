1d25ba9416f2a0f125f8ca979cb8153f03ac6774
Finlay MacDonald <finlay.i.macdonald@btinternet.com>
2025-07-30 01:00:14 +0100

    Added Countdown, Updated dates

---
+++ scripts/countdown.js
@@ -0,0 +1,36 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+const releaseDate = new Date("2026-03-06T12:00:00");
+let now = new Date();
+let diff = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
+});
+
+function updateCountdown() {
+    now = new Date();
+    diff = releaseDate - now;
+
+    if (diff <= 0) {
+        daysEl.textContent = '00';
+        hoursEl.textContent = '00';
+        minutesEl.textContent = '00';
+        secondsEl.textContent = '00';
+        return;
+    }
+
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    //leading zeros
+    daysEl.textContent = days < 10 ? `0${days}` : days;
+    hoursEl.textContent = hours < 10 ? `0${hours}` : hours;
+    minutesEl.textContent = minutes < 10 ? `0${minutes}` : minutes;
+    secondsEl.textContent = seconds < 10 ? `0${seconds}` : seconds;
+}
